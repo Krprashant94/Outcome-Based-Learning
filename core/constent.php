de5ab28c7a943bc6252b2166e59738865e482dd5
Prashant Kumar <kr.prashant94@gmail.com>
2018-11-10 00:45:25 +0530

    Update core/constent.php

--- core/constent.php
+++ core/constent.php
@@ -7,6 +7,7 @@ define("HOME_PAGE", "http://localhost/outcome/Outcome-Based-Learning/");
 
 $_ENV["CSS_PATH"] = HOME_PAGE."assets/css/";
 $_ENV["JS_PATH"] = HOME_PAGE."assets/js/";
+$_ENV["IMG_PATH"] = HOME_PAGE."assets/imgs/";
 $_ENV["BOOTSTRAP_PATH"] = HOME_PAGE."bootstrap/";
 $_ENV["SWEETALERT_PATH"] = HOME_PAGE."sweetalert/";
 $_ENV["JQ_PATH"] = HOME_PAGE."assets/";
