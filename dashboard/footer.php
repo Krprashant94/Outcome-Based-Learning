0064a45d89afaec191ad6abf79394df5ace1f743
Prashant Kumar <kr.prashant94@gmail.com>
2018-11-10 01:58:41 +0530

    new

---
+++ dashboard/footer.php
@@ -0,0 +1,29 @@
+<footer class="footer">
+  <div class="container-fluid">
+    <nav class="float-left">
+      <ul>
+        <li>
+          <a href="https://www.sixpetal.com">
+            Developer Page
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Krprashant94/Outcome-Based-Learning/blob/master/ABOUT.md">
+            About Us
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Krprashant94/Outcome-Based-Learning">
+            GitHub
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <div class="copyright float-right">
+      &copy;
+      <script>
+        document.write(new Date().getFullYear())
+      </script>, made in <i class="material-icons">favorite</i> India.
+    </div>
+  </div>
+</footer>
